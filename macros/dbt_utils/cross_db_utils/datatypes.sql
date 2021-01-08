270e953e5aa54b0d7a8fbe697e614a95e7f63704
Anders Swanson <swanson.anders@gmail.com>
2021-01-07 23:26:31 -0800

    synapse does not support timestamp

--- macros/dbt_utils/cross_db_utils/datatypes.sql
+++ macros/dbt_utils/cross_db_utils/datatypes.sql
@@ -2,6 +2,13 @@
     VARCHAR
 {%- endmacro -%}
 
+-- TEMP UNTIL synapse is standalone adapter type
+{% macro sqlserver__type_timestamp() %}
+    {# Synapse does not support timestamp datatype see: #}
+    {# https://docs.microsoft.com/en-us/azure/synapse-analytics/sql-data-warehouse/sql-data-warehouse-tables-data-types#unsupported-data-types #}
+    datetime
+{% endmacro %}
+
 {#
     Imagine an adapter plugin, dbt-synapse, that inherits from dbt-sqlserver.
     For the time being, we need to explicitly reimplement sqlserver__ macros
@@ -13,4 +20,11 @@
 #}
 {% macro synapse__type_string(field) %}
     {% do return(sqlserver__type_string()) %}
+{% endmacro %}
+
+
+{% macro synapse__type_timestamp() %}
+    {# Synapse does not support timestamp datatype see: #}
+    {# https://docs.microsoft.com/en-us/azure/synapse-analytics/sql-data-warehouse/sql-data-warehouse-tables-data-types#unsupported-data-types #}
+    datetime
 {% endmacro %}

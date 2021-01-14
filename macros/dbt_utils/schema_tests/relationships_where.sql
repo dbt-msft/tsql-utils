49c41a380973b31d5f681598dfac6c44dbf7c0da
Anders Swanson <swanson.anders@gmail.com>
2021-01-13 16:19:01 -0800

    more context

--- macros/dbt_utils/schema_tests/relationships_where.sql
+++ macros/dbt_utils/schema_tests/relationships_where.sql
@@ -3,7 +3,8 @@
   {% set column_name = kwargs.get('column_name', kwargs.get('from')) %}
   {% set from_condition = kwargs.get('from_condition', "1=1") %}
   {% set to_condition = kwargs.get('to_condition', "1=1") %}
-  {# override generic default #}
+  {# override dbt-utils' integration tests args default see: #}
+  {# https://github.com/fishtown-analytics/dbt-utils/blob/bbba960726667abc66b42624f0d36bbb62c37593/integration_tests/models/schema_tests/schema.yml#L67-L75 #}
   {# TSQL has non-ANSI not-equal sign #}
   {% if from_condition == 'id <> 4' %}
       {% set where = 'id != 4' %}
--- macros/dbt_utils/schema_tests/test_unique_where.sql
+++ macros/dbt_utils/schema_tests/test_unique_where.sql
@@ -1,7 +1,8 @@
 {% macro sqlserver__test_unique_where(model) %}
   {% set column_name = kwargs.get('column_name', kwargs.get('arg')) %}
   {% set where = kwargs.get('where', kwargs.get('arg')) %}
-  {# override generic default #}
+  {# override dbt-utils' integration tests args default see: #}
+  {# https://github.com/fishtown-analytics/dbt-utils/blob/bbba960726667abc66b42624f0d36bbb62c37593/integration_tests/models/schema_tests/schema.yml#L53-L65 #}
   {# TSQL has no bool type #}
   {% if where == '_deleted = false' %}
       {% set where = '_deleted = 0' %}

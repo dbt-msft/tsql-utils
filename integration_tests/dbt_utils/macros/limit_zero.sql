11f2060cdbf3c42efa1cb10a8f52c6dd15f1c58f
Anders <swanson.anders@gmail.com>
2021-04-26 23:40:10 -0700

    Auto pause and resume (#37)
    * new version
    
    * auto pause
    
    * azure cli to pause and resume
    
    * use service principal
    
    * per version 0.19.0
    
    * columnstore not for serverless
    
    * yaml formatting
    
    * full dispatch
    
    * auto-start and auto-pause
    
    * dbt_project.yml as SSOT
    
    * missing kwargs
    
    * disable data test on synapse
    
    * trim ws
    
    * fix args kwargs & defaults

--- integration_tests/dbt_utils/macros/limit_zero.sql
+++ integration_tests/dbt_utils/macros/limit_zero.sql
@@ -1,3 +1,7 @@
 {% macro sqlserver__limit_zero() %}
   {{ return('where 0=1') }} 
 {% endmacro %}
+
+{% macro synapse__limit_zero() -%}
+  {{ return(sqlserver__limit_zero()) }}
+{%- endmacro %}
--- macros/dbt_utils/schema_tests/expression_is_true.sql
+++ macros/dbt_utils/schema_tests/expression_is_true.sql
@@ -21,6 +21,7 @@ from validation_errors
 
 {% endmacro %}
 
-{% macro synapse__test_expression_is_true(model, condition='1=1') %}
-    {% do return( tsql_utils.sqlserver__test_expression_is_true(model, condition='1=1')) %}
+{% macro synapse__test_expression_is_true(model, condition) %}
+    {% set expression = kwargs.get('expression', kwargs.get('arg')) %}
+    {% do return( tsql_utils.sqlserver__test_expression_is_true(model, condition, **kwargs)) %}
 {% endmacro %}

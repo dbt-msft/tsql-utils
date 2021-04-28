582e69cacf48b9175554caa5aba334bea314e354
Anders Swanson <swanson.anders@gmail.com>
2021-04-27 17:20:09 -0700

    only synapse is incompatible

--- macros/dbt_utils/cross_db_utils/datediff.sql
+++ macros/dbt_utils/cross_db_utils/datediff.sql
@@ -1,13 +1,7 @@
-{% macro sqlserver__datediff(first_date, second_date, datepart) %}
-
+{% macro synapse__datediff(first_date, second_date, datepart) %}
     datediff(
         {{ datepart }},
         cast({{first_date}} as datetime),
         cast({{second_date}} as datetime)
         )
-
-{% endmacro %}
-
-{% macro synapse__datediff(first_date, second_date, datepart) %}
-    {% do return( tsql_utils.sqlserver__datediff(first_date, second_date, datepart)) %}
 {% endmacro %}

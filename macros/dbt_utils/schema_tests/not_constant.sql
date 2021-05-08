37691fd3867abaa07cfe3214723e4e5c0b82531c
Anders Swanson <swanson.anders@gmail.com>
2021-05-08 17:02:16 -0400

    cleaner formatting

--- macros/dbt_utils/schema_tests/not_constant.sql
+++ macros/dbt_utils/schema_tests/not_constant.sql
@@ -1,25 +1,19 @@
 
 {% macro sqlserver__test_not_constant(model) %}
 
-{% set column_name = kwargs.get('column_name', kwargs.get('arg')) %}
+    {% set column_name = kwargs.get('column_name', kwargs.get('arg')) %}
 
-select count(*)
-
-from (
-
-    select
-          count(distinct {{ column_name }}) as filler_column
-
-    from {{ model }}
-
-    having count(distinct {{ column_name }}) = 1
-
-    ) validation_errors
-
+    select count(*) from (
+        select
+            count(distinct {{ column_name }}) as filler_column
+        from {{ model }}
+        having count(distinct {{ column_name }}) = 1
+        ) validation_errors
 
 {% endmacro %}
 
 {% macro synapse__test_not_constant(model) %}
     {% set column_name = kwargs.get('column_name', kwargs.get('arg')) %}
+
     {% do return( tsql_utils.sqlserver__test_not_constant(model, **kwargs)) %}
 {% endmacro %}

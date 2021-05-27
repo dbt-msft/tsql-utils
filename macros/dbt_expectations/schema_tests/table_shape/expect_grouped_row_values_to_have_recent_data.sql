787cfc13076fd96bf911fe81f47b1f90a93feb57
Alieu <alieusanneh78@yahoo.com>
2021-05-27 14:30:09 -0700

    port  dbt-expect... macros to sql server & synapse

---
+++ macros/dbt_expectations/schema_tests/table_shape/expect_grouped_row_values_to_have_recent_data.sql
@@ -0,0 +1,31 @@
+{% macro sqlserver__test_expect_grouped_row_values_to_have_recent_data(model, group_by, timestamp_column, datepart, interval) %}
+with latest_grouped_timestamps as (
+
+    select
+        {%- for g in group_by %}
+        {{ g }},
+        {%- endfor %}
+        max({{ timestamp_column }}) as latest_timestamp_column
+    from
+        {{ model }}
+        group by 
+            {%- for g in group_by %}
+                 {{g}}{%- if not loop.last %}, {%- endif %}
+            {%- endfor %}
+
+),
+validation_errors as (
+
+    select *
+    from
+        latest_grouped_timestamps
+    where
+        latest_timestamp_column < {{ dbt_utils.dateadd(datepart, interval * -1, dbt_date.now()) }}
+
+)
+select count(*) from validation_errors
+{% endmacro %}
+
+{% macro synapse__test_expect_grouped_row_values_to_have_recent_data(model, group_by, timestamp_column, datepart, interval) %}
+     {% do return( tsql_utils.sqlserver__test_expect_grouped_row_values_to_have_recent_data(model, group_by, timestamp_column, datepart, interval)) %}
+{% endmacro %}

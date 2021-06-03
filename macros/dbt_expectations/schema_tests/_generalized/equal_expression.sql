c2f3b30af0cc62f8ea0ff07b30c14288c351e5cf
Alieu <alieusanneh78@yahoo.com>
2021-06-03 08:35:56 -0700

    thanks b-per?

--- macros/dbt_expectations/schema_tests/_generalized/equal_expression.sql
+++ macros/dbt_expectations/schema_tests/_generalized/equal_expression.sql
@@ -1,8 +1,10 @@
 {%- macro sqlserver__get_select(model, expression, row_condition, group_by) %}
     select
-        {% for g in group_by -%}
-            {{ g }} as col_{{ loop.index }},
-        {% endfor -%}
+        {# {%- if group_by %} #}
+            {% for g in group_by or [] -%}
+                {{ g }} as col_{{ loop.index }},
+            {% endfor -%}
+        {# {% endif %} #}
         {{ expression }} as expression
     from
         {{ model }}
@@ -10,11 +12,13 @@
     where
         {{ row_condition }}
     {% endif %}
-    {%- if group_by|length > 1 or group_by[0] != "'col'" %}
-    group by
-        {% for g in group_by -%}
-            {{ g }}{% if not loop.last %},{% endif %}
-        {% endfor %}
+    {%- if group_by %}
+        {%- if group_by|length > 1 or group_by[0] != "'col'" %}
+        group by
+            {% for g in group_by -%}
+                {{ g }}{% if not loop.last %},{% endif %}
+            {% endfor %}
+        {% endif %}
     {% endif %}
 {% endmacro -%}
 
@@ -34,7 +38,7 @@
     {%- set compare_row_condition = row_condition if not compare_row_condition else compare_row_condition -%}
     {%- set compare_group_by = group_by if not compare_group_by else compare_group_by -%}
 
-    {%- set n_cols = group_by|length %}
+    {%- set n_cols = group_by|length if group_by else 0 %}
     with a as (
         {{ dbt_expectations.get_select(model, expression, row_condition, group_by) }}
     ),
@@ -53,12 +57,16 @@
             abs(coalesce(a.expression, 0) - coalesce(b.expression, 0))/
                 nullif(a.expression, 0) as expression_difference_percent
         from
-            a
-            full outer join
-            b on
-            {% for i in range(1, n_cols + 1) -%}
-                a.col_{{ i }} = b.col_{{ i }} {% if not loop.last %}and{% endif %}
-            {% endfor -%}
+            {% if n_cols > 0 %}
+                a
+                full outer join
+                b on
+                {% for i in range(1, n_cols + 1) -%}
+                    a.col_{{ i }} = b.col_{{ i }} {% if not loop.last %}and{% endif %}
+                {% endfor -%}
+            {% else %}
+                a cross join b
+            {% endif %}
     )
     -- DEBUG:
     -- select * from final

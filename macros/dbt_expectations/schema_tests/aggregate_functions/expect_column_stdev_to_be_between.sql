7f41fa58987dd3e096e18b9938fd48449637e8e8
swanderz <swanson.anders@gmail.com>
2021-06-13 15:05:53 -0400

    add groupby argument

--- macros/dbt_expectations/schema_tests/aggregate_functions/expect_column_stdev_to_be_between.sql
+++ macros/dbt_expectations/schema_tests/aggregate_functions/expect_column_stdev_to_be_between.sql
@@ -1,6 +1,7 @@
 {% macro sqlserver__test_expect_column_stdev_to_be_between(model, column_name,
                                                     min_value,
                                                     max_value,
+                                                    group_by,
                                                     row_condition=None
                                                     ) -%}
 {% set expression %}
@@ -10,6 +11,7 @@ stdev({{ column_name }})
                                         expression=expression,
                                         min_value=min_value,
                                         max_value=max_value,
+                                        group_by_columns=group_by,
                                         row_condition=row_condition
                                         ) }}
 {% endmacro %}
@@ -17,11 +19,13 @@ stdev({{ column_name }})
 {% macro synapse__test_expect_column_stdev_to_be_between(model, column_name,
                                                     min_value,
                                                     max_value,
+                                                    group_by,
                                                     row_condition
                                                     ) -%}
     {% do return( tsql_utils.sqlserver__test_expect_column_stdev_to_be_between(model, column_name,
                                                     min_value,
                                                     max_value,
+                                                    group_by,
                                                     row_condition=None
                                                     )) -%}
 {% endmacro %}

a2b5a4645d84db6a2f05c1044da12c9df44c41ff
Anders <swanson.anders@gmail.com>
2021-06-24 13:39:23 -0700

    Update compare_queries.sql

--- macros/dbt_audit_helper/compare_queries.sql
+++ macros/dbt_audit_helper/compare_queries.sql
@@ -72,7 +72,7 @@ summary_stats as (
     group by in_a, in_b
 )
 -- select * from all_records
--- where not (in_a and in_b)
+-- where not (in_a=1 and in_b=1)
 -- order by {{ primary_key ~ ", " if primary_key is not none }} in_a desc, in_b desc
 
 select

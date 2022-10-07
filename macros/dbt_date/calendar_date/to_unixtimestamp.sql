01967c2f3829116768d302ce80a13f76c7ae1215
Sam Debruyn <sam@debruyn.dev>
2022-10-07 18:55:03 +0200

    Revert "Merge pull request #85 from needbrew/patch-2"
    This reverts commit 688a675b9844e3ef01644f9d7096e3c3461f28f4, reversing
    changes made to 5a5eaa5c8d9cf0e17591dbd76175b2c3150f7d07.

--- macros/dbt_date/calendar_date/to_unixtimestamp.sql
+++ macros/dbt_date/calendar_date/to_unixtimestamp.sql
@@ -1,3 +1,3 @@
 {%- macro sqlserver__to_unixtimestamp(timestamp) -%}
-   DATEDIFF_BIG(ns, '1970-01-01 00:00:00', {{ timestamp }})
+    DATEDIFF(s, '1970-01-01 00:00:00', {{ timestamp }})
 {%- endmacro %}
